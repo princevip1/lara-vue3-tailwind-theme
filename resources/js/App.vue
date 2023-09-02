b705feaaf14541d223ad11fd02ba335ff3956708
prince <princevip720@gmail.com>
2023-09-02 23:23:36 +0600

    initial

---
+++ resources/js/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 class="text-sm font-bold underline text-teal-900">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia rerum labore ut molestias iusto, eos consectetur
+        dolor iure recusandae architecto saepe debitis totam suscipit nostrum assumenda molestiae repellat quasi. Atque, in
+        impedit. Sunt a modi, accusamus minima numquam ipsa temporibus sapiente earum sit architecto doloribus molestiae,
+        excepturi veritatis. Voluptate aliquam libero iure cupiditate dolorum recusandae ratione odio repellendus laudantium
+        corrupti facere numquam iste maiores, earum doloribus minima eum reiciendis repudiandae dolor atque! Incidunt
+        dignissimos deserunt maiores hic expedita cum in voluptate odit qui aliquid. Corporis tempora nam ex dolorem
+        numquam, blanditiis nobis expedita nesciunt accusantium tenetur recusandae aut autem? Quia.
+    </h1>
+</template>
